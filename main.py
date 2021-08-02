5cb56c730a0ab8fbd4d33cb87589fbcdbde3d1cc
The Anh <2001202008@gmail.com>
2021-08-02 16:43:24 +0700

    Khong co gi

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model,datasets
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#import CSV using pandas su li csv
+data= pd.read_csv('machine_learning/data_hw.csv')
+data.info()
+data[['Height','Weight']].head(20)
+#Dung Data ve len plt
+x_bar=data['Height']
+y_bar=data['Weight']
+plt.plot(y_bar,x_bar,'ro')
+plt.title('Biểu đồ cân nặng và chiều cao')
+plt.xlabel('Chiều Cao (cm)')
+plt.ylabel('Cân Nặng(kg)')
+plt.show()
+#Bieu do cho thay su tuyen tinh => Chieu cao va can nang ti le thuan => Co the Dung linear regression.
+lnrgs=linear_model.LinearRegression()
+#Chuyển dữ liệu từ pandas sang numpy
+newXbar=np.array(x_bar)
+newYbar=np.array(y_bar)
+#vẽ line
+lnrgs.fit(newYbar.reshape(-1, 1),newXbar) #vì cần dùng mảng 2 chiều nên .reshape()
+plt.plot(y_bar,x_bar,'ro')
+plt.title('Biểu đồ cân nặng và chiều cao')
+plt.xlabel('Chiều Cao (cm)')
+plt.ylabel('Cân Nặng(kg)')
+plt.plot(newYbar,lnrgs.predict(newYbar.reshape(-1, 1)))
+#kiem thu  chieu cao => Can nang
+kiem_thu=(160,165,190)
+for chieucao in kiem_thu:
+  print(lnrgs.predict([[chieucao]]))
